f3033730dc5dd726e310eb74e9f7d3488fe65ffd
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2022-02-21 21:30:10 +0100

    Update CSS + Adding logo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("loaded");
-    let btn = document.querySelector(".material-icons");
+    let btn = document.querySelector("fa-solid fa-bars");
     let el = document.querySelector("nav");
     btn.addEventListener("click", () => {
         el.classList.toggle("open-menu");
